fcb0ede44c2917468bc3c842fd823dc621bba279
Yegor Bugayenko <yegor@tpc2.com>
2014-08-06 12:23:38 +0200

    bin class

--- lib/pdd.rb
+++ lib/pdd.rb
@@ -19,48 +19,22 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-require 'rubygems'
-require 'rake'
-require 'rdoc'
+# PDD main module
+# Author:: Yegor Bugayenko (yegor@teamed.io)
+# Copyright:: Copyright (c) 2014 Yegor Bugayenko
+# License:: MIT
+module PDD
+  # Code base abstraction
+  class Base
+    # Ctor.
+    # +dir+:: Directory with source code
+    def initialize(dir)
+      @dir = dir
+    end
 
-def name
-  @name ||= File.basename(Dir['*.gemspec'].first, '.*')
-end
-
-def version
-  Gem::Specification.load(Dir['*.gemspec'].first).version
-end
-
-task default: [:test, :features, :rubocop]
-
-require 'rake/testtask'
-desc 'Run all unit tests'
-Rake::TestTask.new(:test) do |test|
-  test.libs << 'lib' << 'test'
-  test.pattern = 'test/**/test_*.rb'
-  test.verbose = true
-end
-
-require 'rdoc/task'
-desc 'Build RDoc documentation'
-Rake::RDocTask.new do |rdoc|
-  rdoc.rdoc_dir = 'rdoc'
-  rdoc.title = "#{name} #{version}"
-  rdoc.rdoc_files.include('README*')
-  rdoc.rdoc_files.include('lib/**/*.rb')
-end
-
-require 'rubocop/rake_task'
-desc 'Run RuboCop on the lib and test directories'
-RuboCop::RakeTask.new(:rubocop) do |task|
-  task.fail_on_error = true
-  task.requires << 'rubocop-rspec'
-end
-
-require 'cucumber/rake/task'
-Cucumber::Rake::Task.new(:features) do |t|
-  t.profile = 'travis'
-end
-Cucumber::Rake::Task.new(:"features:html") do |t|
-  t.profile = 'html_report'
+    # Generate XML.
+    def xml
+      '<xml/>'
+    end
+  end
 end
