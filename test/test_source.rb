a8151f0ee10de0a18fa758dc6a892f7c7701d1ad
Yegor Bugayenko <yegor256@gmail.com>
2019-01-28 15:38:10 +0300

    #117 year up

--- test/test_source.rb
+++ test/test_source.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2018 Yegor Bugayenko
+# Copyright (c) 2014-2019 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -25,7 +25,7 @@ require_relative '../lib/pdd/sources'
 
 # Source test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2014-2018 Yegor Bugayenko
+# Copyright:: Copyright (c) 2014-2019 Yegor Bugayenko
 # License:: MIT
 class TestSource < Minitest::Test
   def test_parsing
