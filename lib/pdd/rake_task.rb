5a9c16ff2c130e1efc5fefee31b91437b29b9a91
Yegor Bugayenko <yegor256@gmail.com>
2019-07-09 08:40:32 +0300

    #124 fixed and tested

--- lib/pdd/rake_task.rb
+++ lib/pdd/rake_task.rb
@@ -6,7 +6,8 @@ module PDD
   # Rake task
   class RakeTask < Rake::TaskLib
     def initialize
-      # @todo #114:30m We need to create implementation of this class.
+      # @todo #114:30m We need to create implementation of this class. At
+      #  the moment it's just a stub. Implement it and remove the abort call.
       abort('NOT IMPLEMENTED')
     end
   end
