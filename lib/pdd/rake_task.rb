9ff4023b83610ccd06acce934d321ff2edeec35c
Alexey Vinogradov <vinogradovai@bcs.ru>
2019-07-10 18:40:26 +0700

    #125 Add parsing the name parameter from the rake task.

--- lib/pdd/rake_task.rb
+++ lib/pdd/rake_task.rb
@@ -5,9 +5,25 @@ require 'rake/tasklib'
 module PDD
   # Rake task
   class RakeTask < Rake::TaskLib
-    def initialize
-      # @todo #114:30m We need to create implementation of this class. At
-      #  the moment it's just a stub. Implement it and remove the abort call.
+    attr_accessor :name
+    def initialize(*args, &task_block)
+      # @todo #125:30m Needs to have more parameters to run this task.
+      #  For now, we just have a single parameter - the name.
+      #  Needs more parameters like the xcop rake task has.
+      @name = args.shift || :pdd
+      desc 'Run PDD' unless ::Rake.application.last_description
+      task(name, *args) do |_, task_args|
+        RakeFileUtils.send(:verbose, true) do
+          yield(*[self, task_args].slice(0, task_block.arity)) if block_given?
+          run
+        end
+      end
+    end
+
+    def run
+      # @todo #125:30m need to implement this method.
+      #  For now, it's just a task,
+      #  that accepts one parameter and throws a system error.
       abort('NOT IMPLEMENTED')
     end
   end
--- test/test_rake_task.rb
+++ test/test_rake_task.rb
@@ -6,8 +6,9 @@ require_relative '../lib/pdd/rake_task'
 # Test for RakeTask
 class TestRakeTask < Minitest::Test
   def test_base
+    PDD::RakeTask.new(:pdd1)
     error = assert_raises SystemExit do
-      PDD::RakeTask.new
+      Rake::Task['pdd1'].invoke
     end
     assert_equal('NOT IMPLEMENTED', error.message)
   end
