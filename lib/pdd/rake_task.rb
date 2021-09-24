867c4892c27864f61f9f704ca844f5a679591dda
doldrums <doldrums.317@gmail.com>
2021-09-24 11:57:30 +0300

    [#132] fix puzzle desc length

--- lib/pdd/rake_task.rb
+++ lib/pdd/rake_task.rb
@@ -34,7 +34,7 @@ module PDD
     def run
       # @todo #125:30m need to implement this method.
       #  For now, it's just a task,
-      #  that prints a Running pdd... message to user.
+      #  that prints a simple Running pdd... message to user
       puts 'Running pdd...' unless @quiet
     end
   end
