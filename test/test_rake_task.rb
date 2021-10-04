30db3ecc90a184ccf9b69bb83dd1ead93b865fa6
Ayomide Bakare <bakkareayomideo@gmail.com>
2021-10-04 08:53:27 +0100

    [#178] fix crash - ENOENT

--- test/test_rake_task.rb
+++ test/test_rake_task.rb
@@ -7,8 +7,8 @@ require_relative '../lib/pdd/rake_task'
 class TestRakeTask < Minitest::Test
   def test_basic
     Dir.mktmpdir 'test' do |dir|
-      Dir.chdir(dir)
-      File.write('a.txt', "\x40todo #55 hello!")
+      file = File.join(dir, 'a.txt')
+      File.write(file, "\x40todo #55 hello!")
       PDD::RakeTask.new(:pdd1) do |task|
         task.quiet = true
       end
