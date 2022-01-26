154181ee6561b88930da7704acfa671d1fd089db
doldrums <doldrums.317@gmail.com>
2022-01-27 01:12:20 +0300

    implement c extension checking rules of puzzles function

--- ext/puzzles.c
+++ ext/puzzles.c
@@ -2,31 +2,13 @@
 #include "ruby/encoding.h"
 #include "library.h"
 
-static VALUE string(VALUE self, VALUE value) {
-  Check_Type(value, T_STRING);
 
-  char* pointer_in = RSTRING_PTR(value);
-  char* pointer_out = string_from_library(pointer_in);
-  return rb_str_new2(pointer_out);
+static VALUE check_rules_extension(VALUE self, VALUE value){
+    Check_Type(value, T_STRING);
+    //TODO: implement parsing current line to check if suitable
+    return T_NIL;
 }
 
-static VALUE number(VALUE self, VALUE value) {
-  Check_Type(value, T_FIXNUM);
-
-  int number_in = NUM2INT(value);
-  int number_out = number_from_library(number_in);
-  return INT2NUM(number_out);
-}
-
-static VALUE boolean(VALUE self, VALUE value) {
-  int boolean_in = RTEST(value);
-  int boolean_out = boolean_from_library(boolean_in);
-  if (boolean_out == 1) {
-    return Qtrue;
-  } else {
-    return Qfalse;
-  }
-}
 
 void Init_puzzles(void) {
   VALUE CFromRubyExample = rb_define_module("CFromRubyExample");
