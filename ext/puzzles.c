f65d7a7fbe15097e3c58aa798ece36988ba6d8a9
doldrums <doldrums.317@gmail.com>
2022-01-27 01:15:29 +0300

    implement c extension basic structure

--- ext/puzzles.c
+++ ext/puzzles.c
@@ -9,12 +9,16 @@ static VALUE check_rules_extension(VALUE self, VALUE value){
     return T_NIL;
 }
 
+static VALUE puzzle_collector(VALUE self, VALUE value){
+    //TODO: implement parsing lines to Init_puzzles
+    return T_NIL;
+}
+
 
 void Init_puzzles(void) {
-  VALUE CFromRubyExample = rb_define_module("CFromRubyExample");
-  VALUE NativeHelpers = rb_define_class_under(CFromRubyExample, "NativeHelpers", rb_cObject);
+  VALUE CFromRubyExample = rb_define_module("PuzzlesCollectorExtension");
+  VALUE NativeHelpers = rb_define_class_under(PuzzlesCollectorExtension, "NativeHelpers", rb_cObject);
 
-  rb_define_singleton_method(NativeHelpers, "string", string, 1);
-  rb_define_singleton_method(NativeHelpers, "number", number, 1);
-  rb_define_singleton_method(NativeHelpers, "boolean", boolean, 1);
+  rb_define_singleton_method(NativeHelpers, "check_rules_extension", string, 1);
+  // rb_define_singleton_method(NativeHelpers, "puzzle_collector", number, 1);
 }
