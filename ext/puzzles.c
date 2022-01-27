54ef1e10007ffa5b89907d8941937851f22899bd
doldrums <doldrums.317@gmail.com>
2022-01-27 14:57:01 +0300

    reformat regexp to POSIX format

--- ext/puzzles.c
+++ ext/puzzles.c
@@ -1,9 +1,48 @@
 #include "ruby/ruby.h"
 #include "ruby/encoding.h"
 #include "library.h"
-
+#include <regex.h>
 
 static VALUE check_rules_extension(VALUE self, VALUE value){
+    Check_Type(value, T_STRING);
+
+    regex_t regex;
+    int reti;
+    char msgbuf[100];
+
+    /* Compile regular expressions */
+
+    // original regexp /[^\s]\x40todo/
+    reti = regcomp(&regex, "[^[[:blank:]]]\x40todo", 0);
+    // original regexp /\x40todo(?!\s+#)/
+    reti1 = regcomp(&regex, "\x40todo(?![[:blank:]]+#)", 0);
+    // original regexp /\x40todo\s+#\s/
+    reti2 = regcomp(&regex, "\x40todo[[:blank:]]+#[[:blank:]]", 0);
+    // original regexp /[^\s]TODO:?/
+    reti3 = regcomp(&regex, "[^[[:blank:]]]TODO:?", 0);
+    // original regexp /TODO(?!:?\s+#)/
+    reti4 = regcomp(&regex, "[^[[:blank:]]]TODO:?", 0);
+    // original regexp /TODO:?\s+#\s/
+    reti5 = regcomp(&regex, "TODO:?[[:blank:]]+#[[:blank:]]", 0);
+    if( reti ){ fprintf(stderr, "Could not compile regex\n"); exit(1); }
+
+    /* Execute regular expression */
+    reti = regexec(&regex, "abc", 0, NULL, 0);
+    if( !reti ){
+       puts("Match");
+    }
+    else if( reti == REG_NOMATCH ){
+       puts("No match");
+    }
+    else{
+       regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+       fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+       exit(1);
+    }
+
+    /* Free compiled regular expression if you want to use the regex_t again */
+    regfree(&regex);
+
     Check_Type(value, T_STRING);
     //TODO: implement parsing current line to check if suitable
     return T_NIL;
