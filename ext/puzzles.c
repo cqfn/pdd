350eb16c4f2195540a08c0ea7f1ee5d93df5237c
doldrums <doldrums.317@gmail.com>
2022-01-28 01:06:49 +0300

    implementation of check rules function

--- ext/puzzles.c
+++ ext/puzzles.c
@@ -4,10 +4,16 @@
 #include <regex.h>
 
 static void check_rules_extension(VALUE self, VALUE value){
+
+    Check_Type(value, T_STRING);
+
+    char* verifiable;
+    verifiable = malloc (sizeof(char) * 1000);
+    strcpy( verifiable, value);
+
     regex_t regex;
     int reti;
     char msgbuf[100];
-    Check_Type(value, T_STRING);
 
     char regexpArray[] = {
         "[^[[:space:]]]\x40todo",
@@ -18,24 +24,21 @@ static void check_rules_extension(VALUE self, VALUE value){
         "TODO:?[[:space:]]+#[[:space:]]"
         };
 
-    for (int i = 0, i < 6; ++i){
 
+    for (int i = 0; i < 6; ++i){
         /* Compile regular expressions */
-        reti = regcomp(&regex, regexpArray[i], 0);
-        if( reti ){ fprintf(stderr, "Could not compile regex\n"); exit(1); }
+        reti = regcomp(&regex, &regexpArray[i], 0);
+        if( reti ){ fprintf(stderr, "Could not compile regex\n"); }
 
         /* Execute regular expression */
-        reti = regexec(&regex, value, 0, NULL, 0);
+        reti = regexec(&regex, verifiable, 0, NULL, 0);
         if( !reti ){
-            puts("Match");
+            puts(" TODO found, but puzzle can't be parsed, \n most probably because #{todo} is not followed by a puzzle marker, \n as this page explains: https://github.com/yegor256/pdd#how-to-format");
         }
-        else if( reti == REG_NOMATCH ){
-            puts("No match");
-        }
-        else{
+        else if( reti != REG_NOMATCH ){
             regerror(reti, &regex, msgbuf, sizeof(msgbuf));
             fprintf(stderr, "Regex match failed: %s\n", msgbuf);
-            exit(1);
+            //exit(1);
         }
 
         /* Free compiled regular expression if you want to use the regex_t again */
