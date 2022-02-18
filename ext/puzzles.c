01dc1ed3634a627504badbf1d1040fdb2430f29a
amoriodi <amoriodi@yandex-team.ru>
2022-02-18 16:06:17 +0300

    ext

--- ext/puzzles.c
+++ ext/puzzles.c
@@ -2,61 +2,195 @@
 #include "ruby/encoding.h"
 #include "library.h"
 #include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static void check_rules_extension(VALUE self, VALUE value){
+typedef struct
+{
+    VALUE *array;
+    size_t used;
+    size_t size;
+} DynamicArrayObject;
+
+void initArray(DynamicArrayObject *a, size_t initialSize)
+{
+    a->array = malloc(initialSize * sizeof(int));
+    a->used = 0;
+    a->size = initialSize;
+}
+
+void insertArray(DynamicArrayObject *a, int element)
+{
+    // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+    // Therefore a->used can go up to a->size
+    if (a->used == a->size)
+    {
+        a->size *= 2;
+        a->array = realloc(a->array, a->size * sizeof(int));
+    }
+    a->array[a->used++] = element;
+}
+
+void freeArray(DynamicArrayObject *a)
+{
+    free(a->array);
+    a->array = NULL;
+    a->used = a->size = 0;
+}
+
+static bool check_basic_rules_extension(VALUE value)
+{
 
     Check_Type(value, T_STRING);
 
-    char* verifiable;
-    verifiable = malloc (sizeof(char) * 1000);
-    strcpy( verifiable, value);
+    char *verifiable;
+    verifiable = malloc(sizeof(char) * 1000);
+    strcpy(verifiable, value);
 
     regex_t regex;
     int reti;
     char msgbuf[100];
 
-    char regexpArray[] = {
-        "[^[[:space:]]]\x40todo",
-        "\x40todo(?![[:space:]]+#)",
-        "\x40todo[[:space:]]+#[[:space:]]",
-        "[^[[:space:]]]TODO:?",
-        "TODO(?!:?[[:space:]]+#",
-        "TODO:?[[:space:]]+#[[:space:]]"
-        };
+    char regexpArray[] =
+        {
+            "[^[[:space:]]]@todo",
+            "@todo(?![[:space:]]+#)",
+            "@todo[[:space:]]+#[[:space:]]",
+            "[^[[:space:]]]TODO:?",
+            "TODO(?!:?[[:space:]]+#",
+            "TODO:?[[:space:]]+#[[:space:]]"};
 
-
-    for (int i = 0; i < 6; ++i){
+    for (int i = 0; i < 6; ++i)
+    {
         /* Compile regular expressions */
         reti = regcomp(&regex, &regexpArray[i], 0);
-        if( reti ){ fprintf(stderr, "Could not compile regex\n"); }
+        if (reti)
+        {
+            fprintf(stderr, "Could not compile regex\n");
+        }
 
         /* Execute regular expression */
         reti = regexec(&regex, verifiable, 0, NULL, 0);
-        if( !reti ){
+        if (!reti)
+        {
             puts(" TODO found, but puzzle can't be parsed, \n most probably because #{todo} is not followed by a puzzle marker, \n as this page explains: https://github.com/yegor256/pdd#how-to-format");
+            regfree(&regex);
+            return (false);
         }
-        else if( reti != REG_NOMATCH ){
+        else if (reti != REG_NOMATCH)
+        {
             regerror(reti, &regex, msgbuf, sizeof(msgbuf));
             fprintf(stderr, "Regex match failed: %s\n", msgbuf);
-            //exit(1);
+            regfree(&regex);
+            return (false);
         }
 
         /* Free compiled regular expression if you want to use the regex_t again */
         regfree(&regex);
-
+        return (true);
     }
 }
 
-static VALUE puzzle_collector(VALUE self, VALUE value){
-    //TODO: implement parsing lines to Init_puzzles
-    return T_NIL;
+static bool check_general_puzzle_rule(VALUE obj)
+{
+    Check_Type(value, T_STRING);
+
+    regex_t regex;
+    int reti;
+    char msgbuf[100];
+
+    /* Compile regular expression */
+    reti = regcomp(&regex, "(.*(?:^|[[:space:]]))#{pfx}[[:space:]]+#([[a-zA-Z0-9_]\-\.:/]+)[[:space:]]+(.+)", 0);
+    if (reti)
+    {
+        fprintf(stderr, "Could not compile regex\n");
+        exit(1);
+    }
+
+    /* Execute regular expression */
+    reti = regexec(&regex, line, 0, NULL, 0);
+
+    if (!reti)
+    {
+        \\match return (true);
+    }
+    regfree(&regex);
+    return (false);
 }
 
+static VALUE puzzle_collector(VALUE self, VALUE file, VALUE klass)
+{
+    //TODO: 1 implement parsing lines to Init_puzzles
+    //TODO: 2 add Rescue
+    fprintf(stderr, "Reading puzzles from path...");
+    DynamicArrayObject puzzles;
+    DynamicArrayObject lines;
+    initArray(&puzzles, 0);
+    FILE *myfile = fopen(file, "r");
+    if (myfile == NULL)
+    {
+        printf("Cannot open file.\n");
+        return 1;
+    }
+    else
+    {
+        //Check for number of line
+        int count = 0;
+        do
+        {
+            if (fgetc(myfile) == '\n')
+                count++;
+        } while (fgetc(myfile) != EOF);
 
-void Init_puzzles(void) {
-  VALUE CFromRubyExample = rb_define_module("PuzzlesCollectorExtension");
-  VALUE NativeHelpers = rb_define_class_under(PuzzlesCollectorExtension, "NativeHelpers", rb_cObject);
+        rewind(myfile);
 
-  rb_define_singleton_method(NativeHelpers, "check_rules_extension", string, 1);
-  // rb_define_singleton_method(NativeHelpers, "puzzle_collector", number, 1);
+        for (int i = 0; i < count; i++)
+        {
+            char line[100];
+            fscanf(myfile, "%[^\n]%*c", line);
+            if (check_basic_rules_extension(line))
+            {
+                //                const char *TODO_presets[2];
+                //                TODO_presets[0] = "@todo";
+                //                TODO_presets[1] = "TODO:";
+                char *ret;
+                ret = strcasestr(line, "@todo");
+                if (ret)
+                {
+                    //printf("found substring at address %p\n", ret);
+                    if (check_general_puzzle_rule(line))
+                    {
+                        //TODO: insert intitialization from ruby call func
+                        result = rb_funcall(klass, puzzle("=="), 3, Qnil);
+                        insertArray(&puzzles, result);
+                    }
+                }
+                else
+                {
+                    ret = strcasestr(line, "TODO:");
+                    if (ret)
+                    {
+                        //printf("found substring at address %p\n", ret);
+                        if (check_general_puzzle_rule(line))
+                        {
+                            //TODO: insert intitialization from ruby call func
+                            result = rb_funcall(obj, rb_intern("=="), 1, Qnil);
+                            insertArray(&puzzles, result);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return puzzles;
+}
+
+void Init_puzzles(void)
+{
+    VALUE CFromRubyExample = rb_define_module("PuzzlesCollectorExtension");
+    VALUE NativeHelpers = rb_define_class_under(PuzzlesCollectorExtension, "NativeHelpers", rb_cObject);
+
+    rb_define_singleton_method(NativeHelpers, "basic_check_rules_extension", string, 1);
+    // rb_define_singleton_method(NativeHelpers, "puzzle_collector", number, 1);
 }
--- lib/pdd.rb
+++ lib/pdd.rb
@@ -34,145 +34,145 @@ require_relative 'pdd/rule/roles'
 # Copyright:: Copyright (c) 2014-2020 Yegor Bugayenko
 # License:: MIT
 
-#StackProf.run(mode: :cpu, out: 'stackprof-output.dump') do
-module PDD
-  # If it breaks.
-  class Error < StandardError
-  end
-
-  # If it violates XSD schema.
-  class SchemaError < Error
-  end
-
-  RULES = {
-    'min-estimate' => PDD::Rule::Estimate::Min,
-    'max-estimate' => PDD::Rule::Estimate::Max,
-    'min-words' => PDD::Rule::Text::MinWords,
-    'max-duplicates' => PDD::Rule::MaxDuplicates,
-    'available-roles' => PDD::Rule::Roles::Available
-  }.freeze
-
-  # Get logger.
-  def self.log
-    unless defined?(@logger)
-      @logger = Logger.new(STDOUT)
-      @logger.formatter = proc { |severity, _, _, msg|
-        if severity == 'ERROR'
-          "#{Rainbow(severity).red}: #{msg}\n"
-        else
-          "#{msg}\n"
-        end
-      }
-      @logger.level = Logger::ERROR
-    end
-    @logger
-  end
-
-  class << self
-    attr_writer :logger
-  end
-
-  # Code base abstraction
-  class Base
-    # Ctor.
-    # +opts+:: Options
-    def initialize(opts)
-      @opts = opts
-      PDD.log.level = Logger::INFO if @opts[:verbose]
-      PDD.log.level = Logger::ERROR if @opts[:quiet]
-      PDD.log.info "My version is #{PDD::VERSION}"
-      PDD.log.info "Ruby version is #{RUBY_VERSION} at #{RUBY_PLATFORM}"
+StackProf.run(mode: :cpu, out: 'stackprof-output.dump') do
+  module PDD
+    # If it breaks.
+    class Error < StandardError
     end
 
-    # Generate XML.
-    def xml
-      StackProf.run(mode: :cpu, out: 'stackprof-cpu-myapp.dump', raw: true) do
-        dir = @opts[:source] ? @opts[:source] : Dir.pwd
-        PDD.log.info "Reading #{dir}"
-        require_relative 'pdd/sources'
-        sources = Sources.new(dir)
-        @opts[:exclude]&.each do |p|
-          sources = sources.exclude(p)
-          PDD.log.info "Excluding #{p}"
-        end
-        sanitize(
-          rules(
-            Nokogiri::XML::Builder.new do |xml|
-              xml << "<?xml-stylesheet type='text/xsl' href='#{xsl}'?>"
-              xml.puzzles(attrs) do
-                sources.fetch.each do |source|
-                  source.puzzles.each do |puzzle|
-                    PDD.log.info "Puzzle #{puzzle.props[:id]} " \
+    # If it violates XSD schema.
+    class SchemaError < Error
+    end
+
+    RULES = {
+      'min-estimate' => PDD::Rule::Estimate::Min,
+      'max-estimate' => PDD::Rule::Estimate::Max,
+      'min-words' => PDD::Rule::Text::MinWords,
+      'max-duplicates' => PDD::Rule::MaxDuplicates,
+      'available-roles' => PDD::Rule::Roles::Available
+    }.freeze
+
+    # Get logger.
+    def self.log
+      unless defined?(@logger)
+        @logger = Logger.new(STDOUT)
+        @logger.formatter = proc { |severity, _, _, msg|
+          if severity == 'ERROR'
+            "#{Rainbow(severity).red}: #{msg}\n"
+          else
+            "#{msg}\n"
+          end
+        }
+        @logger.level = Logger::ERROR
+      end
+      @logger
+    end
+
+    class << self
+      attr_writer :logger
+    end
+
+    # Code base abstraction
+    class Base
+      # Ctor.
+      # +opts+:: Options
+      def initialize(opts)
+        @opts = opts
+        PDD.log.level = Logger::INFO if @opts[:verbose]
+        PDD.log.level = Logger::ERROR if @opts[:quiet]
+        PDD.log.info "My version is #{PDD::VERSION}"
+        PDD.log.info "Ruby version is #{RUBY_VERSION} at #{RUBY_PLATFORM}"
+      end
+
+      # Generate XML.
+      def xml
+        # StackProf.run(mode: :cpu, out: 'stackprof-cpu-myapp.dump', raw: true) do
+          dir = @opts[:source] ? @opts[:source] : Dir.pwd
+          PDD.log.info "Reading #{dir}"
+          require_relative 'pdd/sources'
+          sources = Sources.new(dir)
+          @opts[:exclude]&.each do |p|
+            sources = sources.exclude(p)
+            PDD.log.info "Excluding #{p}"
+          end
+          sanitize(
+            rules(
+              Nokogiri::XML::Builder.new do |xml|
+                xml << "<?xml-stylesheet type='text/xsl' href='#{xsl}'?>"
+                xml.puzzles(attrs) do
+                  sources.fetch.each do |source|
+                    source.puzzles.each do |puzzle|
+                      PDD.log.info "Puzzle #{puzzle.props[:id]} " \
                     "#{puzzle.props[:estimate]}/#{puzzle.props[:role]}" \
                     " at #{puzzle.props[:file]}"
-                    render puzzle, xml
+                      render puzzle, xml
+                    end
                   end
                 end
-              end
-            end.to_xml
+              end.to_xml
+            )
           )
+        # end
+      end
+
+      private
+
+      def attrs
+        {
+          'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+          'xsi:noNamespaceSchemaLocation' => "#{host('xsd')}/#{PDD::VERSION}.xsd",
+          'version' => PDD::VERSION,
+          'date' => Time.now.utc.iso8601
+        }
+      end
+
+      def host(suffix)
+        "http://pdd-#{suffix}.teamed.io"
+      end
+
+      def xsl
+        "#{host('xsl')}/#{PDD::VERSION}.xsl"
+      end
+
+      def render(puzzle, xml)
+        props = puzzle.props
+        xml.puzzle do
+          props.map do |k, v|
+            xml.send(:"#{k}", v)
+          end
+        end
+      end
+
+      def rules(xml)
+        doc = Nokogiri::XML(xml)
+        total = 0
+        list = @opts[:rule] || []
+        unless list.select { |r| r.start_with?('max-duplicates:') }.empty?
+          raise PDD::Error, 'You can\'t modify max-duplicates, it\'s always 1'
+        end
+        list.push('max-duplicates:1').map do |r|
+          name, value = r.split(':')
+          rule = RULES[name]
+          raise "Rule '#{name}' doesn't exist" if rule.nil?
+          rule.new(doc, value).errors.each do |e|
+            PDD.log.error e
+            total += 1
+          end
+        end
+        raise PDD::Error, "#{total} errors, see log above" unless total.zero?
+        xml
+      end
+
+      def sanitize(xml)
+        xsd = Nokogiri::XML::Schema(
+          File.read(File.join(File.dirname(__FILE__), '../assets/puzzles.xsd'))
         )
+        errors = xsd.validate(Nokogiri::XML(xml)).map(&:message)
+        errors.each { |e| PDD.log.error e }
+        PDD.log.error(xml) unless errors.empty?
+        raise SchemaError, errors.join('; ') unless errors.empty?
+        xml
       end
-    end
-
-    private
-
-    def attrs
-      {
-        'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-        'xsi:noNamespaceSchemaLocation' => "#{host('xsd')}/#{PDD::VERSION}.xsd",
-        'version' => PDD::VERSION,
-        'date' => Time.now.utc.iso8601
-      }
-    end
-
-    def host(suffix)
-      "http://pdd-#{suffix}.teamed.io"
-    end
-
-    def xsl
-      "#{host('xsl')}/#{PDD::VERSION}.xsl"
-    end
-
-    def render(puzzle, xml)
-      props = puzzle.props
-      xml.puzzle do
-        props.map do |k, v|
-          xml.send(:"#{k}", v)
-        end
-      end
-    end
-
-    def rules(xml)
-      doc = Nokogiri::XML(xml)
-      total = 0
-      list = @opts[:rule] || []
-      unless list.select { |r| r.start_with?('max-duplicates:') }.empty?
-        raise PDD::Error, 'You can\'t modify max-duplicates, it\'s always 1'
-      end
-      list.push('max-duplicates:1').map do |r|
-        name, value = r.split(':')
-        rule = RULES[name]
-        raise "Rule '#{name}' doesn't exist" if rule.nil?
-        rule.new(doc, value).errors.each do |e|
-          PDD.log.error e
-          total += 1
-        end
-      end
-      raise PDD::Error, "#{total} errors, see log above" unless total.zero?
-      xml
-    end
-
-    def sanitize(xml)
-      xsd = Nokogiri::XML::Schema(
-        File.read(File.join(File.dirname(__FILE__), '../assets/puzzles.xsd'))
-      )
-      errors = xsd.validate(Nokogiri::XML(xml)).map(&:message)
-      errors.each { |e| PDD.log.error e }
-      PDD.log.error(xml) unless errors.empty?
-      raise SchemaError, errors.join('; ') unless errors.empty?
-      xml
     end
   end
 end
-#end
