7d717e9b465fe499b19129ec90e58e106b50b7e1
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-12 23:24:37 -0500

    #228: Update copyright info and actions versions

--- lib/pdd/version.rb
+++ lib/pdd/version.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2023 Yegor Bugayenko
+# Copyright (c) 2014-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -20,7 +20,7 @@
 
 # PDD main module.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2014-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2014-2024 Yegor Bugayenko
 # License:: MIT
 module PDD
   VERSION = '0.0.0'.freeze
--- test/test_many.rb
+++ test/test_many.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2023 Yegor Bugayenko
+# Copyright (c) 2014-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -25,7 +25,7 @@ require_relative '../lib/pdd/sources'
 
 # Test many puzzles to make sure their IDs are correct.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2014-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2014-2024 Yegor Bugayenko
 # License:: MIT
 class TestMany < Minitest::Test
   def test_parsing
